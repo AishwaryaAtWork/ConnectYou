c78ebf67faaec24221693a0026225481ceb2302e
Ayush <ayushaggarwal42003@gmail.com>
2023-11-26 19:14:11 +0530

    Comment out firebase initializeApp import statement

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -27,7 +27,7 @@ export const storage = getStorage(app);
 export const db = getFirestore(app);
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
